6414806cd0b533e4eb545da9e10bfde0c757d5a8
leegallagher001 <lee.gallagher.writing@outlook.com>
2026-09-08 23:27:15 +0100

    first commit

---
+++ seanote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    printf("S E A N O T E\n\n");
+    printf("The Notes App in C\n\n");
+    printf("Main Menu:\n\n");
+    printf("1. NEW NOTE\n");
+    printf("2. SAVED NOTES\n");
+    printf("3. EXIT\n\n");
+    return 0;
+}
