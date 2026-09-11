6cd103960cb7cbc97da5814178fe6cd46da8a243
leegallagher001 <lee.gallagher.writing@outlook.com>
2026-09-11 22:28:58 +0100

    Started work on the "New Note" menu option. Still a long way to go though - need to figure out strings using C :D

--- seanote.c
+++ seanote.c
@@ -3,12 +3,41 @@
 
 void new_note() {
     system("cls");
-    printf("\nS E A N O T E - New Note\n");
+
+    printf("\nS E A N O T E - New Note\n\n");
+
+    printf("Entry Name: ");
+    char entry_name[240];
+    scanf("%s", entry_name[240]);
+    printf("\n\n");
+
+    printf("Entry Date: ");
+    char entry_date[16];
+    scanf("%s", entry_date[16]);
+    printf("\n\n");
+
+    printf("Entry Content: ");
+    char entry_content[2048];
+    scanf("%s", entry_content[2048]);
+    printf("\n\n");
+
+    printf("Review Entry? (Y/N): ");
+    char review_choice;
+    scanf("%c", &review_choice);
+
+    if (review_choice == 'Y' || review_choice == 'y') {
+        printf("\n\nYOUR ENTRY\n\n");
+        printf("%s\n\n", entry_name[240]);
+        printf("%s\n\n", entry_date[16]);
+        printf("%s\n\n", entry_content[2048]);
+    } else {
+        printf("\n\nEntry Discarded\n");
+    }
 }
 
 void saved_notes() {
     system("cls");
-    printf("\nS E A N O T E - Saved Notes\n");
+    printf("\nS E A N O T E - Saved Notes\n\n");
 }
 
 void application_exit() {
