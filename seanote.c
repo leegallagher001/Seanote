02bd8a449abd80ba1d23d4d5920e1df5ea1ccaf2
leegallagher001 <lee.gallagher.writing@outlook.com>
2026-09-13 14:14:36 +0100

    Fixed an issue I was having with the New Note input by using fgets() instead of scanf(). Still far from what I want it to look like but its a start.

--- seanote.c
+++ seanote.c
@@ -1,51 +1,58 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void new_note() {
+void new_note()
+{
     system("cls");
 
-    printf("\nS E A N O T E - New Note\n\n");
+    printf("S E A N O T E - New Note\n\n");
 
     printf("Entry Name: ");
     char entry_name[240];
-    scanf("%s", entry_name[240]);
+    fgets(entry_name, 240, stdin);
     printf("\n\n");
 
     printf("Entry Date: ");
     char entry_date[16];
-    scanf("%s", entry_date[16]);
+    fgets(entry_date, 16, stdin);
     printf("\n\n");
 
     printf("Entry Content: ");
     char entry_content[2048];
-    scanf("%s", entry_content[2048]);
+    fgets(entry_content, 2048, stdin);
     printf("\n\n");
 
     printf("Review Entry? (Y/N): ");
     char review_choice;
     scanf("%c", &review_choice);
 
-    if (review_choice == 'Y' || review_choice == 'y') {
+    if (review_choice == 'Y' || review_choice == 'y')
+    {
         printf("\n\nYOUR ENTRY\n\n");
-        printf("%s\n\n", entry_name[240]);
-        printf("%s\n\n", entry_date[16]);
-        printf("%s\n\n", entry_content[2048]);
-    } else {
+        printf("%s\n\n", entry_name);
+        printf("%s\n\n", entry_date);
+        printf("%s\n\n", entry_content);
+    }
+    else
+    {
         printf("\n\nEntry Discarded\n");
     }
 }
 
-void saved_notes() {
+void saved_notes()
+{
     system("cls");
     printf("\nS E A N O T E - Saved Notes\n\n");
 }
 
-void application_exit() {
+void application_exit()
+{
     system("cls");
     printf("\nUnder Construction\n");
 }
 
-int main() {
+int main()
+{
     printf("S E A N O T E\n\n");
     printf("The Notes App in C\n\n");
 
@@ -57,17 +64,19 @@ int main() {
     printf("Enter Your Choice: ");
     int choice;
     scanf("%d", &choice);
+    getchar();
 
-    switch (choice) {
-        case 1:
-            new_note();
-            break;
-        case 2:
-            saved_notes();
-            break;
-        case 3:
-            application_exit();
-            break;
+    switch (choice)
+    {
+    case 1:
+        new_note();
+        break;
+    case 2:
+        saved_notes();
+        break;
+    case 3:
+        application_exit();
+        break;
     }
 
     return 0;
